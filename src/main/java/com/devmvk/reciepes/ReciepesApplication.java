54bed6b61d5ff55c44974c07dfc5ca29ce0724ef
devmvk <developer.varunkumar@gmail.com>
2022-01-17 22:53:59 +0530

    recepie project setup

---
+++ src/main/java/com/devmvk/reciepes/ReciepesApplication.java
@@ -0,0 +1,13 @@
+package com.devmvk.reciepes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciepesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciepesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devmvk/reciepes/ReciepesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmvk.reciepes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciepesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
