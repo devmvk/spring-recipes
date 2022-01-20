03a13d94b2e65a8c99525841d71fe08b0c954158
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-20 23:21:31 +0530

    POJO created

--- src/main/java/com/devmvk/reciepes/domain/Notes.java
+++ src/main/java/com/devmvk/reciepes/domain/Notes.java
@@ -1,4 +1,34 @@
 package com.devmvk.reciepes.domain;
 
 public class Notes {
+
+    private Long id;
+
+    private String recipeNotes;
+
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
 }
