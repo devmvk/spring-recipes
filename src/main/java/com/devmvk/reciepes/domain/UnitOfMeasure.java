0d7018856939fcb78667ef2dab2fd35db7df835d
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-21 20:40:10 +0530

    Unit odf measure POJO class

---
+++ src/main/java/com/devmvk/reciepes/domain/UnitOfMeasure.java
@@ -0,0 +1,23 @@
+package com.devmvk.reciepes.domain;
+
+public class UnitOfMeasure {
+
+    private Long id;
+    private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}
