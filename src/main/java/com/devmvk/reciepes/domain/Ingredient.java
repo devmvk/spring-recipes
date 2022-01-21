2c4ff97c43631b71912c1050bc43f034c9ecf0f3
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-21 20:24:52 +0530

    adds basic Ingredient POJO class

---
+++ src/main/java/com/devmvk/reciepes/domain/Ingredient.java
@@ -0,0 +1,44 @@
+package com.devmvk.reciepes.domain;
+
+import java.math.BigDecimal;
+
+public class Ingredient {
+
+    private Long id;
+    private String name;
+    private BigDecimal quantity;
+
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
